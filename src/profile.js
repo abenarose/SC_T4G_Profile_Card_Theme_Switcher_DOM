c1bb08644c11b8bdd7ae245bc30089f515d13d82
abenarose <atiemorosemond11@gmail.com>
2026-07-28 10:13:35 -0400

    added changes

--- src/profile.js
+++ src/profile.js
@@ -4,13 +4,14 @@
 
 // TASK 1: Access the document body element and change its background color to '#0f172a'
 // Your code here...
-
+document.body.style.backgroundColor = "#0f172a";
 
 // TASK 2: Use getElementById to select the element with the ID "main-heading"
 // Change its innerText to your own full name.
 // Your selecting of the element goes here ...
 
 // Your innerText code here...
+let heading = document.getElementById("main-heading");
 
 
 // TASK 3: Use querySelector to grab the element with the class ".badge"
@@ -18,6 +19,9 @@
 // Your selecting of the element goes here ...
 
 // Your styling code here...
+let badge =document.querySelector(".badge");
+badge.style.backgroundColor = "purple";
+badge.style.color = "white";
 
 
 // TASK 4: Use getElementById to select "status-text"
@@ -26,6 +30,11 @@
 // Your selecting of the element goes here ...
 
 // Your innerHTML code here...
+let status = document.getElementById("status-text");
+
+status.innerHTML =
+"Status: <span style=`color: #10b981; font-weight: bold;`> ONLINE</span>";
+
 
 
 // TASK 5: Use querySelector to select the entire ".card-container"
@@ -33,10 +42,20 @@
 // Your selecting of the element goes here ... 
 
 // Your classList code here...
+let card = document.querySelector(".card-container");
+card.classList.add("dark-theme-card");
 
 
 // TASK 6: Select the first "span" tag using getElementsByTagName
 // Use .parentElement to find the card container, and change its border radius to "30px"
 // Your selecting of the element goes here ...
 
-// Your parentElement code here...
+// Your parentElement code here...;
+let firstSpan = document.getElementsByTagName("span")[0];
+
+firstSpan.parentElement.style.borderRadius = "30px";
+// Target the direct parent:
+firstSpan.parentElement.style.borderRadius = "30px";
+
+// OR if you need to reach .card-container from the span:
+firstSpan.parentElement.parentElement.style.borderRadius = "30px";
